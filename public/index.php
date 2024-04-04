f0958ff1310c371c756e15749980aa2ca8adc4af
EcharpeRouge <zawiczrczarny@gmail.com>
2024-04-04 13:32:22 +0200

    Update

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+require_once dirname(__DIR__) .'/vendor/autoload.php';
+
+echo 'salut';
+var_dump($_SERVER);
+var_dump($_GET);
+ ?>
---
+++ src/model/Quizz.php
@@ -0,0 +1,6 @@
+<?php
+namespace app\quizz\model;
+
+class Quizz{
+
+}
